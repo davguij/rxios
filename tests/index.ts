5a9c93dab8ba5380e4a9f3dc0f0fdd73b1d13371
David Guijarro <guijarro.dav@gmail.com>
2017-11-30 17:19:35 +0100

    refactor: no more mocks for tests

--- tests/index.ts
+++ tests/index.ts
@@ -1,16 +1,22 @@
 import { rxios } from '../src';
+import * as moxios from 'moxios';
 
 describe('GET methods', () => {
   let rxiosInstance: rxios;
   
   beforeEach(() => {
-    rxiosInstance = new rxios({
-      baseURL: 'http://localhost:3000'
-    });
+    rxiosInstance = new rxios();
+    moxios.install(rxiosInstance._httpClient);
   });
 
   it('makes a successful GET req', () => {
-    const expected = { id: 1, title: 'json-server', author: 'typicode' };
+    const expected = { id: 1, title: 'rxios is so cool!', author: 'davguij' };
+
+    moxios.wait(() => {
+      let request = moxios.requests.mostRecent();
+      request.respondWith({response: expected, status: 200});  
+    });
+
     rxiosInstance.get('/posts/1').subscribe(resp => {
       expect(resp).toMatchObject(expected);
     }, err => {
@@ -19,7 +25,12 @@ describe('GET methods', () => {
   });
 
   it('throws an error on a failed GET req', () => {
-    rxiosInstance.get('/posts/99999').subscribe(resp => {
+    moxios.wait(() => {
+      let request = moxios.requests.mostRecent();
+      request.respondWith({status: 404});  
+    });
+
+    rxiosInstance.get('/posts/1').subscribe(resp => {
       expect(resp).toBeFalsy();
     }, err => {
       expect(err).toBeDefined();
@@ -27,36 +38,42 @@ describe('GET methods', () => {
     });
   });
 
-  it('accepts queryParams', () => {
-    rxiosInstance.get('/posts', {title: 'json-server', author: 'typicode'}).subscribe(resp => {
-      expect(resp).toBeDefined();
-    }, err => {
-      expect(err).toBeFalsy();
-    })
-  })
+  // it('accepts queryParams', () => {
+  //   rxiosInstance.get('/posts', {title: 'rxios is so cool!', author: 'davguij'}).subscribe(resp => {
+  //     expect(resp).toBeDefined();
+  //   }, err => {
+  //     expect(err).toBeFalsy();
+  //   })
+  // })
+
+  afterEach(() => {
+    // import and pass your custom axios instance to this method
+    moxios.uninstall(rxiosInstance._httpClient);
+  });
+
 });
 
-const getRandomInt = (min: number, max: number) => {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
+// const getRandomInt = (min: number, max: number) => {
+//   return Math.floor(Math.random() * (max - min + 1)) + min;
+// }
 
-
-describe('POST methods', () => {
-  let rxiosInstance: rxios;
+// describe('POST methods', () => {
+//   let rxiosInstance: rxios;
   
-  beforeEach(() => {
-    rxiosInstance = new rxios({
-      baseURL: 'http://localhost:3000'
-    });
-  });
+//   beforeEach(() => {
+//     rxiosInstance = new rxios({
+//       baseURL: 'http://localhost:3000'
+//     });
+//   });
 
-  it('makes a successful POST req', () => {
-    const randomId = getRandomInt(0, 99998);
-    const body = { id: randomId, title: 'json-server', author: 'typicode' };
-    rxiosInstance.post('/posts', body).subscribe(postResponse => {
-      expect(postResponse).toMatchObject(body);
-    }, err => {
-      expect(err).toBeFalsy();      
-    });
-  });
-});
+//   it('makes a successful POST req', () => {
+//     const randomId = getRandomInt(0, 99998);
+//     const body = { id: randomId, title: 'json-server', author: 'davguij' };
+//     rxiosInstance.post('/posts', body).subscribe(postResponse => {
+//       expect(postResponse).toMatchObject(body);
+//     }, err => {
+//       expect(err).toBeFalsy();      
+//     });
+//   });
+
+// });
