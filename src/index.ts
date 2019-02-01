07458b9d9f543b53b8868e4a2c9c05c410de9aae
Jason-Morales <jason.morales@imaginelearning.com>
2019-02-01 11:53:52 -0500

    Full response added via new param on queries

--- src/index.ts
+++ src/index.ts
@@ -2,68 +2,68 @@ import axios, { AxiosInstance, AxiosRequestConfig, AxiosPromise } from 'axios';
 import { Observable } from 'rxjs/Observable';
 
 export interface rxiosConfig extends AxiosRequestConfig {
-  localCache?: boolean;
+	localCache?: boolean;
 }
 
 class rxios {
-  private _httpClient: AxiosInstance;
-  
-  constructor(private options: rxiosConfig = {}) {
-    this._httpClient = axios.create(options);
-  }
+	private _httpClient: AxiosInstance;
 
-  private _makeRequest<T>(method: string, url: string, queryParams?: object, body?: object) {
-    let request: AxiosPromise<T>;
-    switch (method) {
-      case 'GET':
-        request = this._httpClient.get<T>(url, {params: queryParams});
-        break;
-      case 'POST':
-        request = this._httpClient.post<T>(url, body, {params: queryParams});
-        break;
-      case 'PUT':
-        request = this._httpClient.put<T>(url, body, {params: queryParams});
-        break;
-      case 'PATCH':
-        request = this._httpClient.patch<T>(url, body, {params: queryParams});
-        break;
-      case 'DELETE':
-        request = this._httpClient.delete(url, {params: queryParams});
-        break;
-    
-      default:
-        throw new Error('Method not supported');
-    }
-    return new Observable<T>(subscriber => {
-      request.then(response => {
-        subscriber.next(response.data);
-        subscriber.complete();
-      }).catch((err: Error) => {
-        subscriber.error(err);
-        subscriber.complete();
-      });
-    });
-  }
+	constructor(private options: rxiosConfig = {}) {
+		this._httpClient = axios.create(options);
+	}
 
-  public get<T>(url: string, queryParams?: object) {
-    return this._makeRequest<T>('GET', url, queryParams);
-  }
+	private _makeRequest<T>(method: string, url: string, queryParams?: object, body?: object, fullResponse: boolean = false) {
+		let request: AxiosPromise<T>;
+		switch (method) {
+			case 'GET':
+				request = this._httpClient.get<T>(url, {params: queryParams});
+				break;
+			case 'POST':
+				request = this._httpClient.post<T>(url, body, {params: queryParams});
+				break;
+			case 'PUT':
+				request = this._httpClient.put<T>(url, body, {params: queryParams});
+				break;
+			case 'PATCH':
+				request = this._httpClient.patch<T>(url, body, {params: queryParams});
+				break;
+			case 'DELETE':
+				request = this._httpClient.delete(url, {params: queryParams});
+				break;
 
-  public post<T>(url: string, body: object, queryParams?: object) {
-    return this._makeRequest<T>('POST', url, queryParams, body);
-  }
-  
-  public put<T>(url: string, body: object, queryParams?: object) {
-    return this._makeRequest<T>('PUT', url, queryParams, body);
-  }
+			default:
+				throw new Error('Method not supported');
+		}
+		return new Observable<T>(subscriber => {
+			request.then(response => {
+				subscriber.next(fullResponse ? response : response.data);
+				subscriber.complete();
+			}).catch((err: Error) => {
+				subscriber.error(err);
+				subscriber.complete();
+			});
+		});
+	}
 
-  public patch<T>(url: string, body: object, queryParams?: object) {
-    return this._makeRequest<T>('PATCH', url, queryParams, body);
-  }
-  
-  public delete(url: string, queryParams?: object) {
-    return this._makeRequest('DELETE', url, queryParams);
-  }
+	public get<T>(url: string, queryParams?: object, fullResponse?: boolean) {
+		return this._makeRequest<T>('GET', url, queryParams, undefined, fullResponse);
+	}
+
+	public post<T>(url: string, body: object, queryParams?: object, fullResponse?: boolean) {
+		return this._makeRequest<T>('POST', url, queryParams, body, fullResponse);
+	}
+
+	public put<T>(url: string, body: object, queryParams?: object, fullResponse?: boolean) {
+		return this._makeRequest<T>('PUT', url, queryParams, body, fullResponse);
+	}
+
+	public patch<T>(url: string, body: object, queryParams?: object, fullResponse?: boolean) {
+		return this._makeRequest<T>('PATCH', url, queryParams, body, fullResponse);
+	}
+
+	public delete(url: string, queryParams?: object, fullResponse?: boolean) {
+		return this._makeRequest('DELETE', url, queryParams, undefined, fullResponse);
+	}
 }
 
 export {rxios, rxios as Rxios};
